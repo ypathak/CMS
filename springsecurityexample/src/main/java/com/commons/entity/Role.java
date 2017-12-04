e44336f1deb3a202a705ee779b10baaae2f49b26
minesh <minesh.b.java@gmail.com>
2017-12-04 15:34:21 +0530

    Fix DB

--- springsecurityexample/src/main/java/com/commons/entity/Role.java
+++ springsecurityexample/src/main/java/com/commons/entity/Role.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 public class Role extends AbstractEntity{
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long roleid;
 
--- springsecurityexample/src/main/java/com/commons/entity/User.java
+++ springsecurityexample/src/main/java/com/commons/entity/User.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 public class User extends AbstractEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 
